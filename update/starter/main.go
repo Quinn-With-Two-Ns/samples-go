e46c52b92d02594b89cb3108f0ae5522251af31a
Quinn Klassen <klassenq@gmail.com>
2024-05-13 17:18:06 -0700

    Show speculative workflow task can cause non determinism

--- update/starter/main.go
+++ update/starter/main.go
@@ -3,10 +3,22 @@ package main
 import (
 	"context"
 	"log"
+	"time"
 
 	"github.com/temporalio/samples-go/update"
 	"go.temporal.io/sdk/client"
+	"go.temporal.io/sdk/worker"
 )
+
+func createWorker(c client.Client, taskQueue string) worker.Worker {
+	w := worker.New(c, taskQueue, worker.Options{
+		WorkflowPanicPolicy: worker.FailWorkflow,
+	})
+
+	w.RegisterWorkflow(update.SpeculativeWorkflowNonDeterminismRepo)
+
+	return w
+}
 
 func main() {
 	c, err := client.Dial(client.Options{})
@@ -15,35 +27,43 @@ func main() {
 	}
 	defer c.Close()
 
+	w := createWorker(c, "update")
+	w.Start()
+
 	workflowOptions := client.StartWorkflowOptions{
 		ID:        "update-workflow-ID",
 		TaskQueue: "update",
 	}
 
-	we, err := c.ExecuteWorkflow(context.Background(), workflowOptions, update.Counter)
+	we, err := c.ExecuteWorkflow(context.Background(), workflowOptions, update.SpeculativeWorkflowNonDeterminismRepo)
 	if err != nil {
 		log.Fatalln("Unable to execute workflow", err)
 	}
 
 	log.Println("Started workflow", "WorkflowID", we.GetID(), "RunID", we.GetRunID())
 
-	mult := 1
-	for i := 0; i < 10; i++ {
-		addend := mult * i
-		mult *= -1 // flip addend between negative and positive for each iteration
-		handle, err := c.UpdateWorkflow(context.Background(), we.GetID(), we.GetRunID(), update.FetchAndAdd, addend)
+	log.Println("Sending updates to be rejected to trigger a speculative workflow task")
+	for i := 0; i < 3; i++ {
+		handle, err := c.UpdateWorkflow(context.Background(), we.GetID(), we.GetRunID(), "update", true)
 		if err != nil {
 			log.Fatal("error issuing update request", err)
 		}
-		var result int
-		err = handle.Get(context.Background(), &result)
-		if err != nil {
-			log.Printf("fetch_and_add with addend %v failed: %v", addend, err)
-		} else {
-			log.Printf("fetch_and_add with addend %v succeeded: %v", addend, result)
-		}
+		err = handle.Get(context.Background(), nil)
+		log.Printf("Update failed with: %v", err)
 	}
+	log.Println("Sending signal to advance the workflow")
+	if err = c.SignalWorkflow(context.Background(), we.GetID(), we.GetRunID(), update.Done, nil); err != nil {
+		log.Fatalf("failed to send %q signal to workflow: %v", update.Done, err)
+	}
+	log.Println("Waiting 10s to allow the signal to trigger a workflow task")
+	time.Sleep(10 * time.Second)
 
+	log.Println("Restarting worker")
+	w.Stop()
+	w = createWorker(c, "update")
+	w.Start()
+
+	log.Println("Sending signal to trigger replay")
 	if err = c.SignalWorkflow(context.Background(), we.GetID(), we.GetRunID(), update.Done, nil); err != nil {
 		log.Fatalf("failed to send %q signal to workflow: %v", update.Done, err)
 	}
--- update/update.go
+++ update/update.go
@@ -1,7 +1,8 @@
 package update
 
 import (
-	"fmt"
+	"errors"
+	"math/rand"
 
 	"go.temporal.io/sdk/workflow"
 )
@@ -11,34 +12,48 @@ const (
 	Done        = "done"
 )
 
-func Counter(ctx workflow.Context) (int, error) {
+func SpeculativeWorkflowNonDeterminismRepo(ctx workflow.Context) (int, error) {
 	log := workflow.GetLogger(ctx)
+	log.Info("Workflow started")
 	counter := 0
-
 	if err := workflow.SetUpdateHandlerWithOptions(
 		ctx,
-		FetchAndAdd,
-		func(ctx workflow.Context, i int) (int, error) {
-			tmp := counter
-			counter += i
-			log.Info("counter updated", "addend", i, "new-value", counter)
-			return tmp, nil
+		"update",
+		func(ctx workflow.Context, reject bool) (bool, error) {
+			counter++
+			return reject, nil
 		},
-		workflow.UpdateHandlerOptions{Validator: nonNegative},
+		workflow.UpdateHandlerOptions{Validator: validator},
 	); err != nil {
 		return 0, err
 	}
-
+	log.Info("Waiting on workflow length")
+	workflow.Await(ctx, func() bool {
+		return workflow.GetInfo(ctx).GetCurrentHistoryLength() == 6
+	})
+	log.Info("Waiting on signal channel")
+	_ = workflow.GetSignalChannel(ctx, Done).Receive(ctx, nil)
+	// Generate some commands
+	encodedRandom := workflow.SideEffect(ctx, func(ctx workflow.Context) interface{} {
+		//workflowcheck:ignore
+		return rand.Intn(10)
+	})
+	var random int
+	err := encodedRandom.Get(&random)
+	if err != nil {
+		return 0, err
+	}
+	log.Info("Waiting on signal channel for a second time")
 	_ = workflow.GetSignalChannel(ctx, Done).Receive(ctx, nil)
 	return counter, ctx.Err()
 }
 
-func nonNegative(ctx workflow.Context, i int) error {
+func validator(ctx workflow.Context, reject bool) error {
 	log := workflow.GetLogger(ctx)
-	if i < 0 {
-		log.Debug("Rejecting negative update", "addend", i)
-		return fmt.Errorf("addend must be non-negative (%v)", i)
+	if reject {
+		log.Debug("Rejecting update")
+		return errors.New("rejecting update")
 	}
-	log.Debug("Accepting update", "addend", i)
+	log.Debug("Accepting update")
 	return nil
 }
